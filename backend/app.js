2f71d696b007b6694d8864e554d3c732154e2a95
zakerous <m.z.humayoon@gmail.com>
2025-08-17 20:13:03 +0300

    initializing routers

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,31 @@
 const express = require("express")
 const connectToDb = require("./utils/connectToDb")
+const loginRouter = require("./controllers/loginRouter")
+const reviewRouter = require("./controllers/reviewRouter")
+const userRouter = require("./controllers/userRouter")
+const bookRouter = require("./controllers/bookRouter")
+const middleware = require("./utils/middleware")
 
 const app = express()
 connectToDb()
 
 app.use(express.json())
+app.use(express.static("dist"))
+app.use(middleware.requestLogger)
+
+app.use("/login", loginRouter)
+app.use("/review", reviewRouter)
+app.use("/user", userRouter)
+app.use("/book", bookRouter)
 
 app.get("/", (req, res) => {
   return res.json({ message: "welcome!" }).end()
 })
+
+// Catch the rest of the endpoints
+app.use(middleware.unknownEndpoint)
+
+// Error Handler
+app.use(middleware.errorHandler)
 
 module.exports = app
