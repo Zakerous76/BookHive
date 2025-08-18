cf624177abeabab31a0e010248d06f9e0ee1eb99
zakerous <m.z.humayoon@gmail.com>
2025-08-18 08:45:29 +0300

    login implemented

--- backend/controllers/loginRouter.js
+++ backend/controllers/loginRouter.js
@@ -1,3 +1,29 @@
 const loginRouter = require("express").Router()
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const User = require("../models/user")
+const { JWT_SECRET } = require("../utils/config")
+
+loginRouter.post("/", async (req, res) => {
+  const { username, password } = req.body
+  try {
+    const user = await User.findOne({ username })
+    if (!user) {
+      return res.status(404).json({ error: `"${username}" not found` })
+    }
+    const passwordHash = user.passwordHash
+
+    const isPasswordCorrect = await bcrypt.compare(password, passwordHash)
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ error: `Provided password is not correct` })
+    }
+    const userForToken = { username, userId: user.id, email: user.email }
+    const token = jwt.sign(userForToken, JWT_SECRET)
+    console.log(`Welcome, ${username}!`)
+    return res.status(200).json({ token })
+  } catch (error) {
+    return res.status(404).json({ error: `${error}` })
+  }
+})
 
 module.exports = loginRouter
