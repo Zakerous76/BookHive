c4e18dd59360b388578f5a1be12c570301c7f28d
zakerous <m.z.humayoon@gmail.com>
2025-08-18 09:27:01 +0300

    refactor

--- backend/controllers/userRouter.js
+++ backend/controllers/userRouter.js
@@ -2,6 +2,31 @@ const userRouter = require("express").Router()
 const bcrypt = require("bcrypt")
 const User = require("../models/user")
 
+const jwt = require("jsonwebtoken")
+const { JWT_SECRET } = require("../utils/config")
+
+userRouter.post("/login", async (req, res) => {
+  const { username, password } = req.body
+  try {
+    const user = await User.findOne({ username })
+    if (!user) {
+      return res.status(404).json({ error: `"${username}" not found` })
+    }
+    const passwordHash = user.passwordHash
+
+    const isPasswordCorrect = await bcrypt.compare(password, passwordHash)
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ error: `Provided password is not correct` })
+    }
+    const userForToken = { username, userId: user.id, email: user.email }
+    const token = jwt.sign(userForToken, JWT_SECRET)
+    console.log(`Welcome, ${username}!`)
+    return res.status(200).json({ token })
+  } catch (error) {
+    return res.status(404).json({ error: `${error}` })
+  }
+})
+
 userRouter.get("/", async (req, res) => {
   const users = await User.find({}).populate("reviews")
   return res.status(200).json(users)
