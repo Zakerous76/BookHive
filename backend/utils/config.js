756d1c6505fc6ef3ed54bed45cc1d4219510a07d
zakerous <m.z.humayoon@gmail.com>
2025-08-18 08:45:01 +0300

    added a secret

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -9,4 +9,6 @@ process.env.NODE_ENV === "production"
 const MONGODB_URI = process.env.MONGODB_URI
 const PORT = process.env.PORT
 
-module.exports = { MONGODB_URI, PORT }
+const JWT_SECRET = process.env.JWT_SECRET
+
+module.exports = { MONGODB_URI, PORT, JWT_SECRET }
