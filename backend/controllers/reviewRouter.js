cdfb511a99563a2ca1bb5d6c4df2e4a5b4d24029
zakerous <m.z.humayoon@gmail.com>
2025-08-18 08:47:37 +0300

    not yet done

--- backend/controllers/reviewRouter.js
+++ backend/controllers/reviewRouter.js
@@ -1,3 +1,24 @@
 const reviewRouter = require("express").Router()
+const Review = require("../models/review")
+
+reviewRouter.get("/", async (req, res) => {
+  try {
+    const reviews = await Review.find({}).populate("User")
+    return res.status(200).json(reviews)
+  } catch (error) {
+    return res.status(404).json(error)
+  }
+})
+
+reviewRouter.post("/create", async (req, res) => {
+  const payload = req.body
+  const review = new Review(payload)
+  try {
+    const savedReview = await review.save()
+    return res.status(201).json(savedReview)
+  } catch (error) {
+    return res.status(400).json(error)
+  }
+})
 
 module.exports = reviewRouter
